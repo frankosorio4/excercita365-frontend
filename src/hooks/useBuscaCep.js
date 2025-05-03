0e1c09e6fc667df29f64d5ad978145146256197f
Frank Osorio <frankosorio4@gmail.com>
2025-05-02 21:33:27 -0300

    fix: adding try-catch to useBuscaCep.js

--- src/hooks/useBuscaCep.js
+++ src/hooks/useBuscaCep.js
@@ -1,7 +1,17 @@
 async function useBuscaCep(cep) {
-  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-  const data = await response.json();
+  try {
+    if (!cep) {
+      throw new Error("CEP is required");
+    }
+    const response = await fetch(
+      `https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    console.log("Data fetched:", data);
 
-  return data;
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
 }
 export default useBuscaCep;
