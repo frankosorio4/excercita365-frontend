d68278978e5f3ef0ac98334ffd6543334a3b9c2a
Frank Osorio <frankosorio4@gmail.com>
2025-05-02 21:57:47 -0300

    Fix: updating cadastroUsuarioForm, styles, form, adding new fields

--- src/components/molecules/CadastroUsuarioForm/index.jsx
+++ src/components/molecules/CadastroUsuarioForm/index.jsx
@@ -1,11 +1,4 @@
-import {
-  Select,
-  TextField,
-  MenuItem,
-  Grid,
-  Button,
-  FormControl
-} from "@mui/material";
+import { Select, TextField, MenuItem, Grid, Button, FormControl } from "@mui/material";
 import { useForm } from "react-hook-form";
 import "./index.css";
 import { Link, useNavigate } from "react-router-dom";
@@ -13,17 +6,9 @@ import { useApiUsuario } from "../../../hooks/useApiUsuario";
 import useBuscaCep from "../../../hooks/useBuscaCep";
 
 function CadastroUsuarioForm() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    getValues,
-    setValue,
-    formState: { errors }
-  } = useForm();
+  const { register, handleSubmit, watch, getValues, setValue, formState: { errors } } = useForm();
 
-  const { cadastrarUsuario, usuarios } = useApiUsuario();
-
+  const { cadastrarUsuario } = useApiUsuario();
   const navigate = useNavigate();
 
   //Validação para quantidade de caracteres no Input de CPF
@@ -47,261 +32,258 @@ function CadastroUsuarioForm() {
   const consultaCep = async () => {
     let cepConsulta = getValues("cep").replace(/\D/g, "");
     if (cepConsulta !== "") {
-      console.log(typeof cepConsulta.length);
       const dadosCep = await useBuscaCep(cepConsulta);
       setValue("logradouro", dadosCep.logradouro);
-      setValue("municipio", dadosCep.localidade);
-      setValue("uf", dadosCep.uf);
+      setValue("bairro", dadosCep.bairro);
+      setValue("cidade", dadosCep.localidade);
+      setValue("estado", dadosCep.uf);
     }
   };
 
   return (
-    <>
-      <Grid className="containerCadastroUsuario">
-        <Grid className="cadastroFormUsuario" sx={{ flexDirection: "column" }}>
-          <form>
-            <Grid className="logoCadastroUsuario">
-              <img src="/assets/logo-exercita365.png" alt="Logo Exercita365" />
-            </Grid>
-            <Grid className="gridNomeUsuario" sx={{ flexDirection: "column" }}>
-              <TextField
-                type="text"
-                variant="outlined"
-                placeholder="Nome"
-                error={!!errors.nome}
-                helperText={errors.nome?.message}
-                sx={{ height: "1rem", marginBottom: "35px" }}
-                {...register("nome", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 100,
-                    message: "Este campo aceita no máximo 100 caracteres."
-                  }
-                })}
-              />
-            </Grid>
-            <Grid className="dadosComplementares">
-              <FormControl fullWidth>
-                <Select
-                  defaultValue="Selecione"
-                  className="customSelect"
-                  {...register("sexo", {
-                    required: "Este campo é obrigatório."
-                  })}>
-                  <MenuItem value="Selecione" disabled className="customMenuItem">
-                    <em>Selecione o Sexo</em>
-                  </MenuItem>
-                  <MenuItem value="Masculino">Masculino</MenuItem>
-                  <MenuItem value="Feminino">Feminino</MenuItem>
-                </Select>
-              </FormControl>
+    <div className="containerCadastroUsuario">
+      <Grid className="cadastroFormUsuario" >
+        <form onSubmit={handleSubmit(
+            sendCadastro, (formErrors) => 
+            console.log('[DEBUG] Erros de validação:', formErrors))} >
+          <Grid className="logoCadastroUsuario">
+            <img src="/assets/logo-exercita365.png" alt="Logo Exercita365" />
+          </Grid>
+          <Grid className="gridNomeUsuario">
+            <TextField
+              placeholder="Nome"
+              type="text"
+              variant="outlined"
+              error={!!errors.nome}
+              helperText={errors.nome?.message}
+              sx={{ mb: 0, width: "100%" }}
+              {...register("nome", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 100,
+                  message: "Este campo aceita no máximo 100 caracteres."
+                }
+              })}
+            />
+          </Grid>
+          <Grid className="dadosComplementares">
 
-              <TextField
-                placeholder="CPF"
-                variant="outlined"
-                error={!!errors.cpf}
-                helperText={errors.cpf?.message}
-                {...register("cpf", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 11,
-                    message: "Este campo aceita no máximo 11 caracteres."
-                  }
-                })}
-                onInput={(event) => handleInput(event, 11)}
-              />
-              <TextField
-                placeholder="Data de Nascimento"
-                type="date"
-                variant="outlined"
-                error={!!errors.dataNascimento}
-                helperText={errors.dataNascimento?.message}
-                className="customDate"
-                {...register("dataNascimento", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 10,
-                    message: "Este campo aceita no máximo 8 caracteres."
-                  }
-                })}
-              />
+            <FormControl fullWidth>
+              <Select
+                defaultValue="Selecione"
+                className="customSelect"
+                {...register("sexo", {
+                  required: "Este campo é obrigatório."
+                })}>
+                <MenuItem value="Selecione" disabled className="customMenuItem">
+                  <em>Selecione o Sexo</em>
+                </MenuItem>
+                <MenuItem value="Masculino">Masculino</MenuItem>
+                <MenuItem value="Feminino">Feminino</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              placeholder="CPF"
+              variant="outlined"
+              error={!!errors.cpf}
+              helperText={errors.cpf?.message}
+              {...register("cpf", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 11,
+                  message: "Este campo aceita no máximo 11 caracteres."
+                }
+              })}
+              onInput={(event) => handleInput(event, 11)}
+            />
 
-              <TextField
-                type="email"
-                placeholder="E-mail"
-                variant="outlined"
-                error={!!errors.email}
-                helperText={errors.email?.message}
-                autoComplete="username"
-                {...register("email", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 60,
-                    message: "Este campo aceita no máximo 60 caracteres."
-                  }
-                })}
-              />
-              <TextField
-                type="password"
-                autoComplete="current-password"
-                placeholder="Senha"
-                error={!!errors.password}
-                helperText={errors.password?.message}
-                {...register("password", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 20,
-                    message: "Este campo aceita no máximo 20 caracteres."
-                  }
-                })}
-              />
+            <TextField
+              placeholder="Data de Nascimento"
+              type="date"
+              variant="outlined"
+              error={!!errors.dataNascimento}
+              helperText={errors.dataNascimento?.message}
+              className="customDate"
+              {...register("dataNascimento", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 10,
+                  message: "Este campo aceita no máximo 8 caracteres."
+                }
+              })}
+            />
 
-              <TextField
-                type="password"
-                autoComplete="current-password"
-                placeholder="Confirma Senha"
-                error={!!errors.confirmaPassword}
-                helperText={errors.confirmaPassword?.message}
-                {...register("confirmaPassword", {
-                  required: "Confirme a Password.",
-                  validate: (value) =>
-                    value === watch("password") || "As senhas não coincidem"
-                })}
-              />
-            </Grid>
+            <TextField
+              type="email"
+              placeholder="E-mail"
+              variant="outlined"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              autoComplete="username"
+              {...register("email", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 60,
+                  message: "Este campo aceita no máximo 60 caracteres."
+                }
+              })}
+            />
+            <TextField
+              type="password"
+              autoComplete="current-password"
+              placeholder="Senha"
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              {...register("password", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 20,
+                  message: "Este campo aceita no máximo 20 caracteres."
+                }
+              })}
+            />
 
-            <Grid className="dadosEndereco">
-              <TextField
-                placeholder="CEP"
-                variant="outlined"
-                error={!!errors.cep}
-                helperText={errors.cep?.message}
-                {...register("cep", {
-                  required: "Este campo é obrigatório.",
-                  onBlur: () => consultaCep(),
-                  maxLength: {
-                    value: 8,
-                    message: "Este campo aceita no máximo 8 caracteres."
-                  }
-                })}
-                onInput={(event) => handleInput(event, 8)}
-              />
+            <TextField
+              type="password"
+              autoComplete="current-password"
+              placeholder="Confirma Senha"
+              error={!!errors.confirmaPassword}
+              helperText={errors.confirmaPassword?.message}
+              {...register("confirmaPassword", {
+                required: "Confirme a Password.",
+                validate: (value) =>
+                  value === watch("password") || "As senhas não coincidem"
+              })}
+            />
+          </Grid>
 
-              <TextField
-                type="text"
-                variant="outlined"
-                placeholder="Logradouro"
-                error={!!errors.logradouro}
-                helperText={errors.logradouro?.message}
-                {...register("logradouro", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 30,
-                    message: "Este campo aceita no máximo 100 caracteres."
-                  }
-                })}
-              />
+          <Grid className="dadosEndereco">
+            <TextField
+              placeholder="CEP"
+              variant="outlined"
+              error={!!errors.cep}
+              helperText={errors.cep?.message}
+              {...register("cep", {
+                required: "Este campo é obrigatório.",
+                onBlur: () => consultaCep(),
+                maxLength: {
+                  value: 8,
+                  message: "Este campo aceita no máximo 8 caracteres."
+                }
+              })}
+              onInput={(event) => handleInput(event, 8)}
+            />
 
-              <TextField
-                type="text"
-                variant="outlined"
-                placeholder="Município"
-                error={!!errors.municipio}
-                helperText={errors.municipio?.message}
-                {...register("municipio", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 30,
-                    message: "Este campo aceita no máximo 30 caracteres."
-                  }
-                })}
-              />
+            <TextField
+              placeholder="Estado"
+              type="text"
+              variant="outlined"
+              error={!!errors.estado}
+              helperText={errors.estado?.message}
+              {...register("estado", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 2,
+                  message: "Este campo aceita no máximo 2 caracteres."
+                }
+              })}
+            />
+            <TextField
+              placeholder="Cidade"
+              type="text"
+              variant="outlined"
+              error={!!errors.localidade}
+              helperText={errors.localidade?.message}
+              {...register("cidade", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 30,
+                  message: "Este campo aceita no máximo 30 caracteres."
+                }
+              })}
+            />
+            <TextField
+              placeholder="bairro"
+              variant="outlined"
+              type="text"
+              error={!!errors.bairro}
+              helperText={errors.bairro?.message}
+              {...register("bairro", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 30,
+                  message: "Este campo aceita no máximo 100 caracteres."
+                }
+              })}
+            />
 
-              <TextField
-                type="text"
-                variant="outlined"
-                placeholder="Estado"
-                error={!!errors.uf}
-                helperText={errors.uf?.message}
-                {...register("uf", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 2,
-                    message: "Este campo aceita no máximo 2 caracteres."
-                  }
-                })}
-              />
+            <TextField
+              placeholder="Logradouro"
+              variant="outlined"
+              type="text"
+              error={!!errors.logradouro}
+              helperText={errors.logradouro?.message}
+              {...register("logradouro", {
+                required: "Este campo é obrigatório.",
+                maxLength: {
+                  value: 50,
+                  message: "Este campo aceita no máximo 50 caracteres."
+                }
+              })}
+            />
 
-              <TextField
-                type="text"
-                variant="outlined"
-                placeholder="Número"
-                error={!!errors.numero}
-                helperText={errors.numero?.message}
-                {...register("numero", {
-                  required: "Este campo é obrigatório.",
-                  maxLength: {
-                    value: 5,
-                    message: "Este campo aceita no máximo 5 caracteres."
-                  }
-                })}
-              />
+            <TextField
+              placeholder="Número de casa"
+              type="text"
+              variant="outlined"
+              error={!!errors.numeroCasa}
+              helperText={errors.numeroCasa?.message}
+              {...register("numeroCasa", {
+                maxLength: {
+                  value: 5,
+                  message: "Este campo aceita no máximo 5 caracteres."
+                }
+              })}
+            />
 
-              <TextField
-                type="text"
-                variant="outlined"
-                placeholder="Complemento"
-                error={!!errors.complemento}
-                helperText={errors.complemento?.message}
-                {...register("complemento", {
-                  maxLength: {
-                    value: 100,
-                    message: "Este campo aceita no máximo 100 caracteres."
-                  }
-                })}
-              />
-            </Grid>
-            <Grid
-              className="containerButtonCadastroUsuario"
-              sx={{ flexDirection: "column" }}>
+            <TextField
+              placeholder="Complemento"
+              type="text"
+              variant="outlined"
+              error={!!errors.complemento}
+              helperText={errors.complemento?.message}
+              {...register("complemento", {
+                maxLength: {
+                  value: 100,
+                  message: "Este campo aceita no máximo 100 caracteres."
+                }
+              })}
+            />
+          </Grid>
+          <Grid
+            className="containerButtonCadastroUsuario"
+            sx={{ flexDirection: "column" }}>
+            <Button
+              type="submit"
+              className="buttonCadastrar"
+              variant="contained"
+              size="medium"
+              sx={{ }}>
+              Cadastrar
+            </Button>
+            <Link to="/login">
               <Button
-                onClick={handleSubmit(sendCadastro)}
-                className="buttonCadastrar"
+                className="buttonVoltar"
                 variant="contained"
                 size="medium"
-                sx={{
-                  fontSize: {
-                    xs: "0.8rem",
-                    sm: "1rem",
-                    md: "1.2rem",
-                    lg: "1.2rem",
-                    xl: "1.2rem"
-                  }
-                }}>
-                Cadastrar
+                sx={{ }}>
+                Já Possui Cadastro?
               </Button>
-              <Link to="/login">
-                <Button
-                  className="buttonVoltar"
-                  variant="contained"
-                  size="medium"
-                  sx={{
-                    fontSize: {
-                      xs: "0.8rem",
-                      sm: "1rem",
-                      md: "1.2rem",
-                      lg: "1.2rem",
-                      xl: "1.2rem"
-                    }
-                  }}>
-                  Já Possui Cadastro?
-                </Button>
-              </Link>
-            </Grid>
-          </form>
-        </Grid>
-      </Grid>
-    </>
+            </Link>
+          </Grid>
+        </form>
+    </Grid>
+    </div >
   );
 }
 
--- src/pages/CadastroUsuarioPage/CadastroUsuarioPage.jsx
+++ src/pages/CadastroUsuarioPage/CadastroUsuarioPage.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable */
-import { Grid } from "@mui/material";
+//import { Grid } from "@mui/material";
 import CadastroUsuarioForm from "../../components/molecules/CadastroUsuarioForm";
-import styles from "./CadastroUsuarioPage.module.css";
+//import styles from "./CadastroUsuarioPage.module.css";
 
 function CadastroUsuarioPage() {
- return (
-  <Grid className={styles.containerUsuarioPrincipal}>
-   <CadastroUsuarioForm />
-  </Grid>
- );
+    return (
+        //<Grid className={styles.containerUsuarioPrincipal}>
+        <>
+            <CadastroUsuarioForm />
+        </>
+    )
 }
 
 export default CadastroUsuarioPage;
